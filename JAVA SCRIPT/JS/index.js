08e0287a42e3a1ecfd5c2f22b85bb7e25370100a
Macaobcn <xavier0@uoc.edu>
2022-02-11 12:46:35 +0100

    JS110222

---
+++ JAVA SCRIPT/JS/index.js
@@ -0,0 +1,7 @@
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+console.log("data d'avui" + d);
+window.alert("Hola Universo");
+//declaració de la variable
+var a;
+//també es pot declarar més d'una variable a  
