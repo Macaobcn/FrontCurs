9750759e673d3a8d2d73268ba428a2cc9c54cb70
Macaobcn <97887721+Macaobcn@users.noreply.github.com>
2022-02-15 07:03:44 +0100

    JS150222

---
+++ JAVA SCRIPT/JS/index2.js
@@ -0,0 +1,19 @@
+//DECLAREM
+let a;
+let b;
+
+//ASSIGNEM
+a=10;
+b=20;
+
+// declarem i assignem
+let c=30;
+
+//calcul
+let total= a + b;
+
+//mostrar por pantalla
+console.log("total " + total);
+document.write("això és el total " + total);
+
+
---
+++ JAVA SCRIPT/JS/index6.js
@@ -0,0 +1,7 @@
+//DECLAREM
+let x = "total" + 16 + 4;
+console.log(x)
+
+
+x= "total" + (16 + 4);
+console.log(x)
