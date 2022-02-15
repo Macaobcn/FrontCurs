228faaf5f8a542719796b40ffdb4c4207fc07a76
Macaobcn <xavier0@uoc.edu>
2022-02-15 13:34:39 +0100

    project150222

---
+++ JAVA SCRIPT/JS/index4.js
@@ -0,0 +1,9 @@
+
+var numero = "2";
+var esNumero = !isNaN(numero);
+if (esNumero) {
+    window.alert(numero + " no és un nombre")
+    document.getElementById("demo").innerHTML = numero + " no és un nombre"
+} else {
+    window.alert
+}
---
+++ JAVA SCRIPT/JS/index5.js
@@ -0,0 +1,22 @@
+
+
+
+//ASSIGNEM
+let a="10534,89";
+let b= 53.2;
+let c=7
+let totalFloor= Math.floor(b); 
+let totalRound= Math.round(b); 
+let totalRandom= Math.random(b); 
+
+
+document.getElementById("demo").innerHTML =  totalRound;
+
+document.getElementById("demo").innerHTML = Math.random();
+
+
+
+//mostrar por pantalla
+console.log("demo " + total);
+document.write("demo " + total)
+
