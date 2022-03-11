aa49af67360f6146273909cae2939dfcaa9565e1
Macaobcn <xavier0@uoc.edu>
2022-03-11 12:25:09 +0100

    110322b

---
+++ ARRAYS/JS/data.js
@@ -0,0 +1,66 @@
+var products = [{
+    id: 1,
+    nombre: 'Monitor 20 Pulgadas',
+    tipo: 'Informática',
+    precio: 500
+    },
+    {
+    id: 2,
+    nombre: 'Televisión 50 Pulgadas',
+    tipo: 'Electrodomésticos',
+    precio: 700
+    },
+    {
+    id: 3,
+    nombre: 'Tablet',
+    tipo:'Informática',
+    precio: 300
+    },
+    {
+    id: 4,
+    nombre: 'Audifonos',
+    tipo:'Electrónica',
+    precio: 200,
+    
+    },
+    {
+    id: 5,
+    nombre: 'Teclado',
+    tipo:'Informática',
+    precio: 50
+    },
+    {
+    id: 6,
+    nombre: 'Celular',
+    tipo: 'Móviles',
+    precio: 500
+    },
+    {
+    id: 7,
+    nombre: 'Bocinas',
+    tipo: 'Varios',
+    precio: 300
+    },
+    {
+    id: 8,
+    nombre: 'Laptop',
+    tipo: 'Informática',
+    precio: 800
+    },
+    
+   ];
+
+   var cart = {
+    informática: {
+    precio: 0,
+    discount: 0
+    },
+    varios: {
+    value: 0,
+    discount: 0
+    },
+    móviles: {
+    value: 0,
+    discount: 0
+    },
+    };
---
+++ ARRAYS/JS/index.js
@@ -0,0 +1,22 @@
+function buy(id){
+    productBuy=products.find(function(element){
+        if(element.id== id){
+            return element;
+        }
+    })
+cart.push(productBuy)
+console.table(cart);
+}
+
+function subtotal(porTipo){
+    for (let i = 0; i < cart.length; i++) {
+        const element = cart[i];
+    }
+    switch (grocery) {
+        case value:
+            break;
+
+        default:
+            break;    
+    }
+}
