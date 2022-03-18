68aa081d2216bf6f96a48b481534d7fe76de18e1
Macaobcn <97887721+Macaobcn@users.noreply.github.com>
2022-03-18 09:05:34 +0100

    180322.1

---
+++ Proyecto/JS/index.js
@@ -0,0 +1,24 @@
+
+
+//function ShowCartList() {
+    //console.table(products);
+    //console.table(cartList)
+//
+
+//function showPrices() {
+    //console.table(subtotal);
+//}
+
+//function showCart() {
+   // console.table(cart);
+//}
+//cart.push (products[id-1]);
+//function cart.push(products[id-1]);
+
+function buy(id) {
+    console.table(products[id-1]);
+   }
+
+
+   
+
